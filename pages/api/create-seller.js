7514962d5cf98a4027765c5267d8b5f1b6958873
haulsale <shubhamkushwaha763601@gmail.com>
2025-11-28 00:47:50 +0530

    fix: update import path for supabase client in create-seller API

--- pages/api/create-seller.js
+++ pages/api/create-seller.js
@@ -1,5 +1,5 @@
 // pages/api/seller/create-product.js
-import { supabase } from '../../../lib/supabaseClient'
+import { supabase } from '../../lib/supabaseClient'
 
 export default async function handler(req, res) {
   if (req.method !== 'POST')
